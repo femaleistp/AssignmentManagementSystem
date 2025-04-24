8d48797a867750ab976dda4cb3387fd7d28b7489
Brittany Hancock <femaleentp@gmail.com>
2025-04-24 08:43:26 -0700

    Add project files.

---
+++ AssignmentManagement.ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using AssignmentLibrary;
+
+try
+{
+    // This will fail validation because title is blank
+    var assignment = new Assignment("", "This will fail");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Validation failed!");
+    Console.WriteLine("Message: " + ex.Message);
+}
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine(); // Keeps the console window open
